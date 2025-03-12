243f072b06b8b24882edb7d422a03a7e5f5fbc92
AtharvGaur <atharava112@gmail.com>
2025-03-12 22:05:07 +0530

    Update Navbar component styling and logo; modify SVG fill colors

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,18 +1,20 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
-    <div className='p-4 flex items-center justify-between'>
-    <div className='items-center flex gap-3'>
+    <div className='p-5 flex items-center justify-between'>
+    <div className='items-center flex gap-2 cursor-pointer'>
         <Image src={'/newlogo.svg'} alt='logo' height={30} width={30}/>
-        <h2 className='text-2xl font-bold'>YourReel</h2>
+        <h2 className='text-2xl font-bold'>PosePerfect</h2>
     </div>
     <div className='flex flex-row-reverse gap-3'>
-        {/* <UserButton/>
+        {/* <UserButton/> */}
         <Link href="/dashboard">
         <Button>Dashboard</Button>
-        </Link> */}
+        </Link>
     </div>
     </div>
   )
